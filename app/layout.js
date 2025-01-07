578700a2e0fb671aff2a760cce8dffc7dab7f8d3
FelipeX2001 <levelads@levelads.co>
2025-01-06 21:13:11 -0500

    cambio envs y layaout

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'AICLON - Voice Assistant',
-  description: 'DEMO - AI Voice AICLON SEPHIA',
+  description: 'DEMO - AI AICLON SEPHIA',
 }
 
 export default function RootLayout({ children }) {
